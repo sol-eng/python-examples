fc66f023d7e0e7e4a08ea8beeac1fea055cac7b3
Cole Arendt <cole.arendt@outlook.com>
2019-02-07 19:19:07 -0500

    clean up sentiment analysis api

--- sentiment-analysis/plumber.R
+++ sentiment-analysis/plumber.R
@@ -1,14 +1,12 @@
 library(plumber)
 library(reticulate)
 
-use_python("/opt/python/bin/python3")
-
 #* @apiTitle Sentiment Analysis Text API
 
 source_python('predict.py')
 
 #* @param input Text to classify
 #* @get /predict
-function(input = "") {
+function(input = "This is happy text!") {
     result = predict(input)
 }
